c2c3f7da652ebbd89321974954f99c88b6f40082
JsCodeDevlopment <jonatasilva118@gmail.com>
2025-04-26 12:37:22 -0300

    feat: implement getAll method in CustomersService for retrieving customers with optional filters

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { ListCustomersResponseDto } from 'src/customers/dto/list-customers-response.dto';
+import { FilterCustomerDto } from 'src/customers/types/get-customers-filters.type';
 import { AsaasService } from '../asaas/asaas.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { CustomerResponseDto } from './dto/customer-response.dto';
@@ -9,5 +11,31 @@ export class CustomersService {
 
   create(dto: CreateCustomerDto): Promise<CustomerResponseDto> {
     return this.asaas.request<CustomerResponseDto>('post', '/customers', dto);
+  }
+
+  async getAll(filters?: FilterCustomerDto): Promise<ListCustomersResponseDto> {
+    const query = new URLSearchParams();
+
+    if (filters) {
+      const params: Record<string, string | number> = {
+        ...(filters.name && { name: filters.name }),
+        ...(filters.cpfCnpj && { cpfCnpj: filters.cpfCnpj }),
+        limit:
+          filters.limit !== undefined && filters.limit !== null
+            ? filters.limit
+            : 10,
+        offset:
+          filters.offset !== undefined && filters.offset !== null
+            ? filters.offset
+            : 0,
+      };
+
+      Object.entries(params).forEach(([key, value]) => {
+        query.append(key, value.toString());
+      });
+    }
+
+    const path = query.toString() ? `/customers?${query}` : '/customers';
+    return this.asaas.request<ListCustomersResponseDto>('get', path);
   }
 }
