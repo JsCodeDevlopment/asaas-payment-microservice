561849677d9c9d0bbd17671be1132a87d416424b
JsCodeDevlopment <jonatasilva118@gmail.com>
2025-04-29 17:55:38 -0300

    feat: enhance PaymentsController to support access token and environment parameters in create and findInstallments methods

--- src/payments/payment.controller.ts
+++ src/payments/payment.controller.ts
@@ -1,6 +1,21 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+import {
+  ApiHeader,
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { PaymentsService } from 'src/payments/payment.service';
+import { EnvironmentOptionsType } from 'src/types/environment.enum';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { PaymentResponseDto } from './dto/payment-response.dto';
 
@@ -26,15 +41,47 @@ export class PaymentsController {
   })
   @ApiResponse({ status: 400, description: 'O campo value é obrigatório.' })
   @ApiResponse({ status: 400, description: 'O campo dueDate é obrigatório.' })
-  create(@Body() dto: CreatePaymentDto): Promise<PaymentResponseDto> {
-    return this.svc.create(dto);
+  @ApiHeader({
+    name: 'access_token',
+    description: 'API Key do Asaas para autenticação',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'environment',
+    enum: ['SANDBOX', 'PROD'],
+    description: 'Escolhe o ambiente Asaas (SANDBOX ou PROD)',
+    required: false,
+    example: 'SANDBOX',
+  })
+  create(
+    @Body() dto: CreatePaymentDto,
+    @Headers('access_token') token: string,
+    @Query('enviroment') environment: EnvironmentOptionsType,
+  ): Promise<PaymentResponseDto> {
+    return this.svc.create(dto, token, environment);
   }
 
   @Get('installments/:id')
   @ApiOperation({ summary: 'Lista parcelas de um plano' })
   @ApiResponse({ status: 200, description: 'Parcelas listadas com sucesso.' })
   @ApiResponse({ status: 404, description: 'Plano não encontrado.' })
-  findInstallments(@Param('id') id: string) {
-    return this.svc.findInstallments(id);
+  @ApiHeader({
+    name: 'access_token',
+    description: 'API Key do Asaas para autenticação',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'environment',
+    enum: ['SANDBOX', 'PROD'],
+    description: 'Escolhe o ambiente Asaas (SANDBOX ou PROD)',
+    required: false,
+    example: 'SANDBOX',
+  })
+  findInstallments(
+    @Param('id') id: string,
+    @Headers('access_token') token: string,
+    @Query('enviroment') environment: EnvironmentOptionsType,
+  ) {
+    return this.svc.findInstallments(id, token, environment);
   }
 }
