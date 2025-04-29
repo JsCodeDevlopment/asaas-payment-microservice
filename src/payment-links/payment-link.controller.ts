9ed3e7d40dd8773fd97f65cef6b9f072f2131f82
JsCodeDevlopment <jonatasilva118@gmail.com>
2025-04-29 17:55:31 -0300

    feat: enhance PaymentLinksController to support access token and environment parameters in create, findAll, and findOne methods

--- src/payment-links/payment-link.controller.ts
+++ src/payment-links/payment-link.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ApiHeader, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { EnvironmentOptionsType } from 'src/types/environment.enum';
 import { CreatePaymentLinkDto } from './dto/create-payment-link.dto';
 import { PaymentLinkResponseDto } from './dto/payment-link-response.dto';
 import { PaymentLinksService } from './payment-links.service';
@@ -11,19 +20,66 @@ export class PaymentLinksController {
 
   @Post()
   @ApiOperation({ summary: 'Cria um link de pagamento' })
-  create(@Body() dto: CreatePaymentLinkDto): Promise<PaymentLinkResponseDto> {
-    return this.svc.create(dto);
+  @ApiHeader({
+    name: 'access_token',
+    description: 'API Key do Asaas para autenticação',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'environment',
+    enum: ['SANDBOX', 'PROD'],
+    description: 'Escolhe o ambiente Asaas (SANDBOX ou PROD)',
+    required: false,
+    example: 'SANDBOX',
+  })
+  create(
+    @Body() dto: CreatePaymentLinkDto,
+    @Headers('access_token') token: string,
+    @Query('enviroment') environment: EnvironmentOptionsType,
+  ): Promise<PaymentLinkResponseDto> {
+    return this.svc.create(dto, token, environment);
   }
 
   @Get()
   @ApiOperation({ summary: 'Lista todos os links' })
-  findAll(): Promise<PaymentLinkResponseDto[]> {
-    return this.svc.findAll();
+  @ApiHeader({
+    name: 'access_token',
+    description: 'API Key do Asaas para autenticação',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'environment',
+    enum: ['SANDBOX', 'PROD'],
+    description: 'Escolhe o ambiente Asaas (SANDBOX ou PROD)',
+    required: false,
+    example: 'SANDBOX',
+  })
+  findAll(
+    @Headers('access_token') token: string,
+    @Query('enviroment') environment: EnvironmentOptionsType,
+  ): Promise<PaymentLinkResponseDto[]> {
+    return this.svc.findAll(token, environment);
   }
 
   @Get(':id')
   @ApiOperation({ summary: 'Detalha um link específico' })
-  findOne(@Param('id') id: string): Promise<PaymentLinkResponseDto> {
-    return this.svc.findOne(id);
+  @ApiHeader({
+    name: 'access_token',
+    description: 'API Key do Asaas para autenticação',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'environment',
+    enum: ['SANDBOX', 'PROD'],
+    description: 'Escolhe o ambiente Asaas (SANDBOX ou PROD)',
+    required: false,
+    example: 'SANDBOX',
+  })
+  findOne(
+    @Param('id') id: string,
+    @Headers('access_token') token: string,
+    @Query('enviroment') environment: EnvironmentOptionsType,
+  ): Promise<PaymentLinkResponseDto> {
+    return this.svc.findOne(id, token, environment);
   }
 }
