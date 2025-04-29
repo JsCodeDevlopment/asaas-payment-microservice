d55a5863c5df704742fc108fa223ef83b39ce2f1
JsCodeDevlopment <jonatasilva118@gmail.com>
2025-04-29 17:55:20 -0300

    feat: enhance NotificationsController to support access token and environment parameters in getByCustomer, update, and updateBatch methods

--- src/notifications/notification.controller.ts
+++ src/notifications/notification.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Get, Param, Put } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Param,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { ApiHeader, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { EnvironmentOptionsType } from 'src/types/environment.enum';
 import { NotificationResponseDto } from './dto/notification-response.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
 import { NotificationsService } from './notifications.service';
@@ -11,24 +20,68 @@ export class NotificationsController {
 
   @Get('customer/:id')
   @ApiOperation({ summary: 'Lista notificações de um cliente' })
-  getByCustomer(@Param('id') id: string): Promise<NotificationResponseDto[]> {
-    return this.svc.getByCustomer(id);
+  @ApiHeader({
+    name: 'access_token',
+    description: 'API Key do Asaas para autenticação',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'environment',
+    enum: ['SANDBOX', 'PROD'],
+    description: 'Escolhe o ambiente Asaas (SANDBOX ou PROD)',
+    required: false,
+    example: 'SANDBOX',
+  })
+  getByCustomer(
+    @Param('id') id: string,
+    @Headers('access_token') token: string,
+    @Query('enviroment') environment: EnvironmentOptionsType,
+  ): Promise<NotificationResponseDto[]> {
+    return this.svc.getByCustomer(id, token, environment);
   }
 
   @Put(':id')
   @ApiOperation({ summary: 'Atualiza uma notificação' })
+  @ApiHeader({
+    name: 'access_token',
+    description: 'API Key do Asaas para autenticação',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'environment',
+    enum: ['SANDBOX', 'PROD'],
+    description: 'Escolhe o ambiente Asaas (SANDBOX ou PROD)',
+    required: false,
+    example: 'SANDBOX',
+  })
   update(
     @Param('id') id: string,
     @Body() dto: UpdateNotificationDto,
+    @Headers('access_token') token: string,
+    @Query('enviroment') environment: EnvironmentOptionsType,
   ): Promise<NotificationResponseDto> {
-    return this.svc.update(id, dto);
+    return this.svc.update(id, dto, token, environment);
   }
 
   @Put('batch')
   @ApiOperation({ summary: 'Atualiza notificações em lote' })
+  @ApiHeader({
+    name: 'access_token',
+    description: 'API Key do Asaas para autenticação',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'environment',
+    enum: ['SANDBOX', 'PROD'],
+    description: 'Escolhe o ambiente Asaas (SANDBOX ou PROD)',
+    required: false,
+    example: 'SANDBOX',
+  })
   updateBatch(
     @Body() dto: UpdateNotificationDto[],
+    @Headers('access_token') token: string,
+    @Query('enviroment') environment: EnvironmentOptionsType,
   ): Promise<NotificationResponseDto[]> {
-    return this.svc.updateBatch(dto);
+    return this.svc.updateBatch(dto, token, environment);
   }
 }
