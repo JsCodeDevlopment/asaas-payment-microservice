bd30568322c082fed9d96fda80217ce02c0b1bf5
JsCodeDevlopment <jonatasilva118@gmail.com>
2025-04-29 17:55:25 -0300

    feat: enhance PaymentLinksService to support access token and environment parameters in create, findAll, and findOne methods

--- src/payment-links/payment-links.service.ts
+++ src/payment-links/payment-links.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { EnvironmentOptionsType } from 'src/types/environment.enum';
+import { RequestMethodsEnum } from 'src/types/request-methods.enum';
 import { AsaasService } from '../asaas/asaas.service';
 import { CreatePaymentLinkDto } from './dto/create-payment-link.dto';
 import { PaymentLinkResponseDto } from './dto/payment-link-response.dto';
@@ -7,22 +9,44 @@ import { PaymentLinkResponseDto } from './dto/payment-link-response.dto';
 export class PaymentLinksService {
   constructor(private readonly asaas: AsaasService) {}
 
-  create(dto: CreatePaymentLinkDto): Promise<PaymentLinkResponseDto> {
-    return this.asaas.request<PaymentLinkResponseDto>(
-      'post',
+  create(
+    dto: CreatePaymentLinkDto,
+    token: string,
+    environment: EnvironmentOptionsType = 'SANDBOX',
+  ): Promise<PaymentLinkResponseDto> {
+    return this.asaas.request<PaymentLinkResponseDto, CreatePaymentLinkDto>(
+      RequestMethodsEnum.POST,
       '/paymentLinks',
       dto,
+      token,
+      environment,
     );
   }
 
-  findAll(): Promise<PaymentLinkResponseDto[]> {
-    return this.asaas.request<PaymentLinkResponseDto[]>('get', '/paymentLinks');
+  findAll(
+    token: string,
+    environment: EnvironmentOptionsType = 'SANDBOX',
+  ): Promise<PaymentLinkResponseDto[]> {
+    return this.asaas.request<PaymentLinkResponseDto[], undefined>(
+      RequestMethodsEnum.GET,
+      '/paymentLinks',
+      undefined,
+      token,
+      environment,
+    );
   }
 
-  findOne(id: string): Promise<PaymentLinkResponseDto> {
-    return this.asaas.request<PaymentLinkResponseDto>(
-      'get',
+  findOne(
+    id: string,
+    token: string,
+    environment: EnvironmentOptionsType = 'SANDBOX',
+  ): Promise<PaymentLinkResponseDto> {
+    return this.asaas.request<PaymentLinkResponseDto, undefined>(
+      RequestMethodsEnum.GET,
       `/paymentLinks/${id}`,
+      undefined,
+      token,
+      environment,
     );
   }
 }
