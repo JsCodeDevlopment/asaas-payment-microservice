c6ce26a7484763f6287b84512fcb0b4948bf6644
JsCodeDevlopment <jonatasilva118@gmail.com>
2025-04-30 10:39:14 -0300

    refactor: update Asaas configuration to use ASASS_SANDBOX_BASE_URL and remove commented-out code

--- src/config/asaas.config.ts
+++ src/config/asaas.config.ts
@@ -1,18 +1,15 @@
 import { registerAs } from '@nestjs/config';
 
 export interface AsaasConfig {
-  // apiKey: string;
   baseUrl: string;
   sandboxUrl: string;
 }
 
 export default registerAs('asaas', (): AsaasConfig => {
-  // const apiKey = process.env.ASAAS_API_KEY ?? '';
   const baseUrl = process.env.ASAAS_BASE_URL ?? '';
-  const sandboxUrl = process.env.ASAAS_SANDBOX_URL ?? '';
+  const sandboxUrl = process.env.ASAAS_SANDBOX_BASE_URL ?? '';
 
   return {
-    // apiKey,
     baseUrl,
     sandboxUrl,
   };
