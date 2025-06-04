53541f4e1ecdb18931ffa4f5e9e95b7814b19592
JsCodeDevlopment <jonatasilva118@gmail.com>
2025-06-04 17:09:13 -0300

    feat: integrate AsaasModule into PaymentsModule for enhanced payment processing capabilities

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AsaasModule } from 'src/asaas/asaas.module';
 import { PaymentsController } from './payment.controller';
 import { PaymentsService } from './payment.service';
 import { EncryptionService } from './services/encryption.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, AsaasModule],
   controllers: [PaymentsController],
   providers: [PaymentsService, EncryptionService],
   exports: [PaymentsService, EncryptionService],
