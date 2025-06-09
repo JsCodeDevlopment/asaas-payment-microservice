1a356300c6569b2d2a85cdf814bdcf0ca6c59ba8
JsCodeDevlopment <jonatasilva118@gmail.com>
2025-06-09 17:34:36 -0300

    feat: set global prefix for API routes to enhance route organization

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  // Adiciona o prefixo global /api para todas as rotas
+  app.setGlobalPrefix('api');
+
   setupSwagger(app);
   setupCors(app);
 
